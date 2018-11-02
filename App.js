bfa46115babc0c6a661c18e932f1d818aecdc76f
sureshezhil <sureshiknow@gmail.com>
2018-11-02 18:27:50 +0530

    01112018_06:30

--- App.js
+++ App.js
@@ -1,24 +1,50 @@
-import React, { Component } from 'react';
-import { TouchableOpacity, View, ActivityIndicator, Text, Alert} from 'react-native';
-export default class App extends Component {
-handlePress = async () => {
-  fetch('http://192.168.10.3:8000/data')
-    .then((response) => response.json())
-    .then((responseJson) => {
- Alert.alert("Your data" + responseJson[0].x);
+import React, { Component } from 'react'
+import { View, Text ,Image, Linking} from 'react-native'
 
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
-  render(){
-  return(
-   <View style={{paddingTop: 50, paddingLeft: 50 }}>
-    <TouchableOpacity onPress={this.handlePress.bind(this)}>
-     <Text style={{paddingTop: 50, paddingLeft: 50, color: '#FF0000'}}> Click to View</Text>
-    </TouchableOpacity>
+class HttpExample extends Component {
+   state = {
+      data: []
+   }
+
+   componentDidMount = () => {
+      fetch('http://192.168.10.3:8000/iSee/notifi_data')
+      .then((response) => response.json())
+      .then((responseJson) => {
+         console.warn(responseJson);
+
+         this.setState({
+            data:responseJson
+
+         })
+      })
+      .catch((error) => {
+         console.error("error");
+      });
+   }
+   render() {
+      return (
+<View>
+         { this.state.data.map((item,index) => (
+           <View>
+                                         <Text>
+                                         ID:{item.id}
+                                         </Text>
+                                         <Image
+                               style={{width: 100, height: 100}}
+                               source={{uri:item.image}}
+                             />
+                                         <Text style={{color:'green'}} onPress={ ()=> Linking.openURL('http://192.168.10.3:8000/iSee/accept/'+item.id) } >
+                                         Accept</Text>
+
+                                         <Text  style={{color:'red'}} onPress={ ()=> Linking.openURL('http://192.168.10.3:8000/iSee/reject/'+item.id) } >
+                                         Reject</Text>
+                                         </View>
+
+                                    ))}
+
+
 </View>
-  );
+      )
+   }
 }
-}
+export default HttpExample
