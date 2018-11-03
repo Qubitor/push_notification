a00c293b9c6ac3286699fa05773df4c2e51790cd
sureshezhil <sureshiknow@gmail.com>
2018-11-03 18:35:00 +0530

    03112018_06:30

--- App.js
+++ App.js
@@ -1,29 +1,117 @@
 import React, { Component } from 'react'
-import { View, Text ,Image, Linking} from 'react-native'
+import { View, Text ,Image, Linking,Button} from 'react-native'
+import { TouchableOpacity, ActivityIndicator,Alert} from 'react-native';
+import ReactNativeAN from 'react-native-alarm-notification';
+import {Icon,Header} from 'react-native-elements'
+const fireDate = ReactNativeAN.parseDate(new Date(Date.now() + 1000));
+
+
+const alarmNotifData = {
+    id: "12345",                                  // Required
+    title: "iVigil Smart Alert",               // Required
+    message: "New Person is entered and pushed into waiting list",           // Required
+    channel: "my_channel_id",                     // Required. Same id as specified in MainApplication's onCreate method
+    ticker: "My Notification Ticker",
+    auto_cancel: true,                            // default: true
+    vibrate: true,
+    vibration: 100,                               // default: 100, no vibration if vibrate: false
+    small_icon: "ic_launcher",                    // Required
+    large_icon: "ic_launcher",
+    play_sound: true,
+    sound_name: null,                             // Plays custom notification ringtone if sound_name: null
+    color: "green",
+    schedule_once: true,                          // Works with ReactNativeAN.scheduleAlarm so alarm fires once
+    tag: 'some_tag',
+    fire_date: fireDate,                          // Date for firing alarm, Required for ReactNativeAN.scheduleAlarm.
+
+    // You can add any additional data that is important for the notification
+    // It will be added to the PendingIntent along with the rest of the bundle.
+    // e.g.
+  	data: { foo: "bar" },
+};
 
 class HttpExample extends Component {
    state = {
       data: []
    }
 
+   method(){
+       //Schedule Future Alarm
+       ReactNativeAN.scheduleAlarm(alarmNotifData);
+
+       //Delete Scheduled Alarm
+       ReactNativeAN.deleteAlarm("12345");
+
+       //Stop Alarm
+       ReactNativeAN.stopAlarm();
+
+       //Send Local Notification Now
+       ReactNativeAN.sendNotification(alarmNotifData);
+
+       //Get All Scheduled Alarms
+       ReactNativeAN.getScheduledAlarms().then(alarmNotif=>console.log(alarmNotif));
+
+       //Clear Notification(s) From Notification Center/Tray
+       // ReactNativeAN.removeAllFiredNotifications()
+       // ReactNativeAN.removeFiredNotification("12345")
+
+       //Removes Future Local Notifications
+       // ReactNativeAN.cancelAllNotifications()
+       // ReactNativeAN.cancelNotification("12345")
+   }
+
    componentDidMount = () => {
       fetch('http://192.168.10.3:8000/iSee/notifi_data')
       .then((response) => response.json())
       .then((responseJson) => {
-         console.warn(responseJson);
-
+         if(responseJson.length>0)
+         {
+           this.method()
+         }
          this.setState({
             data:responseJson
 
          })
+
+
       })
       .catch((error) => {
-         console.error("error");
+         console.error(error);
       });
    }
+   handleAccept = async (x) => {
+     fetch('http://192.168.10.3:8000/iSee/accept/'+x)
+       .then((response) => response.json())
+       .then((responseJson) => {
+    Alert.alert("User Accepted Successfully");
+       })
+       .catch((error) => {
+         console.error(error);
+       });
+   }
+   handleReject = async (x) => {
+     fetch('http://192.168.10.3:8000/iSee/reject/'+x)
+       .then((response) => response.json())
+       .then((responseJson) => {
+         Alert.alert("User Rejetced Successfully");
+
+       })
+       .catch((error) => {
+         console.error(error);
+       });
+   }
+
    render() {
       return (
+
+
 <View>
+<Header
+  placement="left"
+  leftComponent={{ icon: 'menu', color: '#fff' }}
+  centerComponent={{ text: 'iVigil', style: { color: '#fff' } }}
+  rightComponent={{ icon: 'home', color: '#fff' }}
+/>
          { this.state.data.map((item,index) => (
            <View>
                                          <Text>
@@ -33,18 +121,23 @@ class HttpExample extends Component {
                                style={{width: 100, height: 100}}
                                source={{uri:item.image}}
                              />
-                                         <Text style={{color:'green'}} onPress={ ()=> Linking.openURL('http://192.168.10.3:8000/iSee/accept/'+item.id) } >
-                                         Accept</Text>
-
-                                         <Text  style={{color:'red'}} onPress={ ()=> Linking.openURL('http://192.168.10.3:8000/iSee/reject/'+item.id) } >
-                                         Reject</Text>
+                                         <TouchableOpacity onPress={this.handleAccept.bind(this,item.id)}>
+                                          <Text style={{fontSize:20, color: 'green'}}>Accept</Text>
+                                         </TouchableOpacity>
+                                         <TouchableOpacity onPress={this.handleReject.bind(this,item.id)}>
+                                          <Text style={{fontSize:20,color: 'red'}}>Reject</Text>
+                                         </TouchableOpacity>
                                          </View>
+
 
                                     ))}
 
 
+
 </View>
+
       )
    }
 }
+
 export default HttpExample
--- android/app/src/main/java/com/push_notification/MainApplication.java
+++ android/app/src/main/java/com/push_notification/MainApplication.java
@@ -3,7 +3,8 @@ package com.push_notification;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.evollu.react.fcm.FIRMessagingPackage;
+import com.ocetnik.timer.BackgroundTimerPackage;
+import com.jamesisaac.rnbackgroundtask.BackgroundTaskPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -11,6 +12,15 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
+
+import com.emekalites.react.alarm.notification.ANPackage;
+
+import android.app.NotificationManager;
+import android.app.NotificationChannel;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -24,7 +34,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FIRMessagingPackage()
+            new BackgroundTimerPackage(),
+            new BackgroundTaskPackage(),
+            new ANPackage()
+
       );
     }
 
@@ -43,5 +56,28 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    BackgroundTaskPackage.useContext(this);
+
+    String id = "my_channel_id";					// The id of the channel.
+    CharSequence name = "my_channel_name";			// The user-visible name of the channel.
+    String description = "my_channel_description";	// The user-visible description of the channel.
+
+    if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationChannel mChannel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_DEFAULT);
+
+      // Configure the notification channel.
+      mChannel.setDescription(description);
+
+      mChannel.enableLights(true);
+      // Sets the notification light color for notifications posted to this
+      // channel, if the device supports this feature.
+      mChannel.setLightColor(Color.RED);
+
+      mChannel.enableVibration(true);
+      mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+
+      NotificationManager mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+      mNotificationManager.createNotificationChannel(mChannel);
+    }
   }
 }
